190cd3887ca0acec3ac14a5c864ee8fcdddb802d
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-12-02 20:57:14 +0700

    Update ClassPageStreamTab.js

--- client/src/components/pages/ClassPage/ClassPageStreamTab.js
+++ client/src/components/pages/ClassPage/ClassPageStreamTab.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import {IconButton, Paper} from "@mui/material";
+import {IconButton, Paper, useTheme} from "@mui/material";
 import Grid from "@mui/material/Grid";
-import {InfoOutlined} from "@mui/icons-material";
+import {CropFree, InfoOutlined, MoreVert} from "@mui/icons-material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
 
 export default function ClassPageStreamTab() {
+    const classCode = "cl75z6n";
+    const className = "2310-CLC-AWP-20KTPM2"
+    const classSubject = "Advanced Web Programming"
 
+    const theme = useTheme();
     const classCover = (
         <Paper elevation={0} sx={{
             backgroundSize: 'cover',
@@ -19,9 +26,9 @@ export default function ClassPageStreamTab() {
         }}>
             <Box sx={{position: 'absolute', left: '2.5rem', bottom: '1rem'}}>
                 <Typography variant='h4'
-                            sx={{fontFamily: 'Google', fontWeight: 500, color: 'white'}}>ASD</Typography>
+                            sx={{fontFamily: 'Google', fontWeight: 500, color: 'white'}}>{className}</Typography>
                 <Typography variant='h6'
-                            sx={{fontFamily: 'Google', fontWeight: 500, color: 'white'}}>asd</Typography>
+                            sx={{fontFamily: 'Google', fontWeight: 500, color: 'white'}}>{classSubject}</Typography>
             </Box>
             <IconButton sx={{color: 'white', position: 'absolute', right: '.5rem', bottom: '.5rem'}}>
                 <InfoOutlined/>
@@ -30,15 +37,45 @@ export default function ClassPageStreamTab() {
     )
 
     const leftColumn = (<Box>
-        <Box sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2}}>
-            Class code
-            <br/>
-            cl75z6n
+        <Box sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2, padding: '1ch 2ch'}}>
+            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                < Typography variant={'subtitle2'} sx={{fontFamily: 'Google', justifyContent: 'space-between'}}>
+                    Class code
+                </Typography>
+                <IconButton><MoreVert/></IconButton>
+            </Box>
+            <Box sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                <Typography variant={'h6'}
+                            sx={{
+                                fontFamily: 'Google',
+                                color: theme.palette.primary.main,
+                                alignContent: 'end'
+                            }}>{classCode}
+                </Typography>
+                <IconButton><CropFree sx={{color: theme.palette.primary.main}}/></IconButton>
+            </Box>
         </Box>
-        <Box mt={3} sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2}}>
-            Upcoming
-            <br/>
-            No work due soon
+        <Box sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2, padding: 2, mt: 3}}>
+            < Typography variant={'subtitle2'} sx={{fontFamily: 'Google', justifyContent: 'space-between'}}>
+                Upcoming
+            </Typography>
+            <Typography variant={'caption'}>
+                No work due soon
+            </Typography>
+            <Button sx={{
+                textTransform: 'none',
+                fontWeight: 600,
+                alignContent: 'end',
+                mb: -1,
+                mr: -1,
+                ml: 'auto',
+                display: 'block'
+            }}>
+                <Typography variant={'subtitle2'}
+                            sx={{fontFamily: 'Google', fontWeight: 500, fontSize: '.875rem'}}>
+                    View all
+                </Typography>
+            </Button>
         </Box>
     </Box>)
 
@@ -47,7 +84,7 @@ export default function ClassPageStreamTab() {
             Announce something to your class
         </Box>
         <Box mt={3} sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2}}>
-This is where you can talk to your class
+            This is where you can talk to your class
         </Box>
     </Box>)
 
@@ -59,8 +96,8 @@ This is where you can talk to your class
             sx={{margin: '0 auto', mx: 'auto', maxWidth: '1000px',}}
         >
             <Grid item xs={12}>{classCover}</Grid>
-            <Grid item xs={3}>{leftColumn} </Grid>
-            <Grid item xs={9}>{rightColumn}</Grid>
+            <Grid item xs={2.5}>{leftColumn} </Grid>
+            <Grid item xs={9.5}>{rightColumn}</Grid>
         </Grid>
     );
 }
