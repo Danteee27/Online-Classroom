19820ff11b56646a92e5d63a71b87c098001348e
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-12-03 17:15:56 +0700

    finish join class by invite link UI

--- client/src/components/layouts/AddClassButton.js
+++ client/src/components/layouts/AddClassButton.js
@@ -11,6 +11,7 @@ import Button from "@mui/material/Button";
 export default function AddClassButton() {
     const [anchorElMenu, setAnchorElMenu] = React.useState(null);
     const [anchorElCreateClass, setAnchorElCreateClass] = React.useState(null);
+    const [anchorElJoinClass, setAnchorElJoinClass] = React.useState(null);
 
     const [className, setClassName] = React.useState("");
     const [classNameError, setClassNameError] = React.useState(false);
@@ -18,6 +19,29 @@ export default function AddClassButton() {
     const [section, setSection] = React.useState("");
     const [subject, setSubject] = React.useState("");
     const [room, setRoom] = React.useState("");
+
+    const [classInviteLink, setClassInviteLink] = React.useState("");
+    const [classInviteLinkError, setClassInviteLinkError] = React.useState(false);
+
+    const handleJoinClass = (event) => {
+        event.preventDefault();
+
+        setClassInviteLinkError(false);
+
+        if (classInviteLink === '') {
+            setClassInviteLinkError(true);
+            return;
+        }
+
+        const data = {
+            classInviteLink: classInviteLink,
+        }
+
+        // TODO: HANDLE JOIN CLASSROOM HERE
+
+        // on complete
+        setAnchorElJoinClass(null);
+    }
 
     const handleCreateClass = (event) => {
         event.preventDefault();
@@ -42,7 +66,35 @@ export default function AddClassButton() {
         setAnchorElCreateClass(null);
     }
 
-    const noticeDialog = (
+    const joinClassForm = (
+        <Dialog
+            fullWidth
+            open={Boolean(anchorElJoinClass)}
+            onClose={() => setAnchorElJoinClass(null)}>
+            <form autoComplete="off" onSubmit={handleJoinClass}>
+                <Box
+                    sx={{
+                        display: 'grid',
+                        gap: '1.25rem',
+                        margin: '1.25rem',
+                    }}
+                ><Typography variant={'h6'} sx={{fontFamily: 'Google', fontSize: 16}}>Join class</Typography>
+                    <TextField label="Class invite link"
+                               required variant="filled"
+                               onChange={e => setClassInviteLink(e.target.value)}
+                               value={classInviteLink}
+                               error={classInviteLinkError}/>
+                    <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <Button onClick={() => setAnchorElJoinClass(null)} color={'inherit'}
+                                sx={{textTransform: 'none', fontFamily: 'Google', fontSize: 14}}>Cancel</Button>
+                        <Button type={"submit"}
+                                sx={{textTransform: 'none', fontFamily: 'Google', fontSize: 14}}>Join</Button>
+                    </div>
+                </Box>
+            </form>
+        </Dialog>)
+
+    const createClassForm = (
         <Dialog
             fullWidth
             open={Boolean(anchorElCreateClass)}
@@ -94,8 +146,7 @@ export default function AddClassButton() {
             vertical: 'top',
             horizontal: 'right',
         }}>
-        <MenuItem onClick={() => {
-        }}>
+        <MenuItem onClick={(e) => setAnchorElJoinClass(e.currentTarget)}>
             Join Class
         </MenuItem>
         <MenuItem onClick={(e) => setAnchorElCreateClass(e.currentTarget)}>
@@ -107,16 +158,14 @@ export default function AddClassButton() {
         <>
             <IconButton
                 size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
                 onClick={(e) => setAnchorElMenu(e.currentTarget)}
                 color="inherit"
             >
                 <Add/>
             </IconButton>
             {addClass}
-            {noticeDialog}
+            {joinClassForm}
+            {createClassForm}
         </>
     );
 }
