6302e7a03a4d93381d5fc61d12f8b674ea50759e
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-12-16 17:46:49 +0700

    Update AssignmentPage.js

--- src/components/pages/AssignmentPage/AssignmentPage.js
+++ src/components/pages/AssignmentPage/AssignmentPage.js
@@ -1,72 +1,120 @@
 import * as React from 'react';
 import {useState} from 'react';
-import {IconButton, Tooltip, useTheme} from "@mui/material";
+import {IconButton, useTheme} from "@mui/material";
 import Grid from "@mui/material/Grid";
-import {Add, ContentCopy, MoreVert, PersonOutlined, Send} from "@mui/icons-material";
+import {Add, AssignmentOutlined, MoreVert, PersonOutlined, Send} from "@mui/icons-material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
+import Divider from "@mui/material/Divider";
 
 export default function AssignmentPage() {
-    const classCode = "cl75z6n";
-
-    const theme = useTheme();
+    const a = {
+        name: 'Midterm - Authentication',
+        createdAt: 'Nov 15',
+        maxGrade: 100,
+        studentReview: {
+            avatar: '',
+            name: 'abc',
+            reviewTime: 'Dec 3',
+            reason: 'this work contains my ultimate effort. I cannot let it down this easily'
+        },
+        teacherReview: {
+            avatar: '',
+            name: 'Michael',
+            reviewTime: 'Dec 3',
+            reason: 'your opnion is subjective'
+        },
+        studentGrade: 10,
+        description: 'use passport.js'
+    }
 
     const sendReview = () => {
         // todo handle post review
     }
 
-    const leftColumn = (<Box>
-        <Box sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2, padding: '1ch 2ch'}}>
-            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                < Typography variant={'subtitle2'} sx={{fontFamily: 'Google', justifyContent: 'space-between'}}>
-                    Class invite link
-                </Typography>
-                <IconButton><MoreVert/></IconButton>
+    const theme = useTheme();
+    const [review, setReview] = useState("");
+
+    const leftColumn = (
+        <Box sx={{display: 'flex'}}>
+            <Box sx={{
+                background: theme.palette.primary.main,
+                p: '.5rem',
+                width: '40px',
+                height: '40px',
+                borderRadius: 100,
+                mr: '1rem'
+            }}>
+                <AssignmentOutlined sx={{fill: 'white',}}/>
             </Box>
-            <Box sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
-                <Typography variant={'h6'}
+            <Box sx={{flexGrow: 1}}>
+                <Box sx={{display:'flex', justifyContent:'space-between'}}>
+                    <Typography variant='h4' sx={{fontFamily:'Google'}}>{a.name}</Typography>
+                    <IconButton><MoreVert/></IconButton>
+                </Box>
+                <Box sx={{display:'flex'}}><Typography variant={'subtitle2'}
+                               sx={{
+                                   fontFamily: 'Google',
+                                   fontWeight: 500,
+                                   fontSize: '.875rem'
+                               }}>{a.teacherReview.name}</Typography>
+                    <Box sx={{mx: '.325rem'}}>•</Box>
+                    <Typography variant={'subtitle2'}
+                                sx={{
+                                    fontWeight: 500,
+                                    fontSize: '.875rem',
+                                }}>{a.createdAt}</Typography>
+                </Box>
+                <Typography variant={'subtitle2'}
                             sx={{
                                 fontFamily: 'Google',
-                                color: theme.palette.primary.main,
-                                alignContent: 'end'
-                            }}>{classCode}
+                                fontSize: '.875rem',
+                                borderBottom: '0.0625rem solid ' + theme.palette.primary.main,
+                                mb: '1rem',
+                                pb: '1rem'
+                            }}>
+                    <b>{a.studentGrade}</b>/{a.maxGrade}
                 </Typography>
-                <Tooltip title={'copy'}><IconButton><ContentCopy sx={{color: theme.palette.primary.main}}/></IconButton></Tooltip>
+                <Typography variant={'body1'}
+                            sx={{
+                                fontSize: '.875rem',
+                                borderBottom: '0.0625rem solid lightgrey',
+                                mb: '1rem',
+                                pb: '2rem'
+                            }}>
+                    {a.description}
+                </Typography>
+                <Box sx={{display: 'flex'}}><PersonOutlined sx={{fill: theme.palette.primary.main}}/>
+                    <Typography variant={'h6'}
+                                sx={{
+                                    fontSize: '1rem',
+                                    fontFamily: 'Google',
+                                    color: theme.palette.primary.main
+                                }}>
+                        Student reviews
+                    </Typography>
+                </Box>
+                <ChatDialog avatar={a.studentReview.avatar} name={a.studentReview.name} reason={a.studentReview.reason}
+                            time={a.studentReview.reviewTime}/>
+                <ChatDialog avatar={a.teacherReview.avatar} name={a.teacherReview.name} reason={a.teacherReview.reason}
+                            time={a.teacherReview.reviewTime}/>
+                <form autoComplete="off" style={{display: 'flex', paddingTop: '2ch'}} onSubmit={sendReview}>
+                    <Box sx={{'& fieldset': {borderRadius: 100}, flexGrow: 1}}>
+                        <TextField placeholder={'Add private comment...'}
+                                   size="small"
+                                   variant="outlined"
+                                   onChange={e => setReview(e.target.value)}
+                                   value={review}
+                                   InputProps={{style: {fontSize: '.875rem'}}}
+                                   sx={{display: 'flex'}}/>
+                    </Box>
+                    <IconButton type={"submit"}><Send/></IconButton>
+                </form>
             </Box>
-        </Box>
-        <Box sx={{border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2, padding: 2, mt: 3}}>
-            < Typography variant={'subtitle2'} sx={{fontFamily: 'Google', justifyContent: 'space-between'}}>
-                Upcoming
-            </Typography>
-            <Typography variant={'caption'}>
-                No work due soon
-            </Typography>
-            <Button sx={{
-                textTransform: 'none',
-                fontWeight: 600,
-                mb: -1,
-                mr: -1,
-                ml: 'auto',
-                display: 'block'
-            }}>
-                <Typography variant={'subtitle2'}
-                            sx={{fontFamily: 'Google', fontWeight: 500, fontSize: '.875rem'}}>
-                    View all
-                </Typography>
-            </Button>
-        </Box>
     </Box>)
-
-    const studentReview = {
-        avatar: '',
-        name: 'abc',
-        reviewTime: 'Dec 3',
-        reason: 'this work contains my ultimate effort. I cannot let it down this easily'
-    };
-    const teacherReview = {avatar: '', name: 'Michael', reviewTime: 'Dec 3', reason: 'your opnion is subjective'}
 
     function ChatDialog(props) {
         return <Box sx={{display: 'flex', mt: '1rem'}}>
@@ -95,8 +143,6 @@ export default function AssignmentPage() {
             </Box>
         </Box>
     }
-
-    const [review, setReview] = useState("");
 
     const rightColumn = (
         <Box>
@@ -135,36 +181,7 @@ export default function AssignmentPage() {
                 </Button>
             </Box>
 
-            <Box mt={3}
-                 sx={{px: 2, py: 2, border: '0.0625rem solid rgb(218,220,224)', borderRadius: 2,
-                     boxShadow: 3,}}>
-                <Box sx={{display: 'flex'}}><PersonOutlined sx={{fill: theme.palette.primary.main}}/>
-                    <Typography variant={'h6'}
-                                sx={{
-                                    fontSize: '1rem',
-                                    fontFamily: 'Google',
-                                    color: theme.palette.primary.main
-                                }}>
-                        Student reviews
-                    </Typography>
-                </Box>
-                <ChatDialog avatar={studentReview.avatar} name={studentReview.name} reason={studentReview.reason}
-                            time={studentReview.reviewTime}/>
-                <ChatDialog avatar={teacherReview.avatar} name={teacherReview.name} reason={teacherReview.reason}
-                            time={teacherReview.reviewTime}/>
-                <form autoComplete="off" style={{display: 'flex', paddingTop: '2ch'}} onSubmit={sendReview}>
-                    <Box sx={{'& fieldset': {borderRadius: 100}, flexGrow: 1}}>
-                        <TextField placeholder={'Add private comment...'}
-                                   size="small"
-                                   variant="outlined"
-                                   onChange={e => setReview(e.target.value)}
-                                   value={review}
-                                   InputProps={{style: {fontSize: '.875rem'}}}
-                                   sx={{display: 'flex'}}/>
-                    </Box>
-                    <IconButton type={"submit"}><Send/></IconButton>
-                </form>
-            </Box>
+
         </Box>)
 
     return (<Grid
