a7c6a5c26a5b704ddcb4b70b9f613202260c53f9
phattrieu <86005213+phatng45@users.noreply.github.com>
2023-12-19 20:14:25 +0700

    Update ManageAccountsPage.js

--- src/components/pages/ManagePage/ManageAccountsPage.js
+++ src/components/pages/ManagePage/ManageAccountsPage.js
@@ -16,9 +16,11 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
+import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
+import axios from "axios";
 
 const renderRole = (cell) => {
-    if (cell.value === 'admin')
+    if (cell.value.name === 'Admin')
         return (<Chip
             avatar={<Assignment sx={{fill: '#1d3f96'}}/>}
             label={'Admin'}
@@ -31,27 +33,9 @@ const renderRole = (cell) => {
                 }
             }}/>);
 
-    if (cell.value === 'teacher')
-        return (<Chip
-            avatar={<svg fill={'#44399f'} focusable="false" width="24" height="24" viewBox="0 0 24 24"
-                         className=" NMm5M">
-                <circle cx="17" cy="12.5" r="2.5"></circle>
-                <path
-                    d="M17 15.62c-1.67 0-5 .84-5 2.5V20h10v-1.88c0-1.66-3.33-2.5-5-2.5zM10 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm1.82 9.17c.01 0 0 0 0 0zM10 13c-2.67 0-8 1.34-8 4v3h8v-2H4v-.99c.2-.72 3.3-2.01 6-2.01.6 0 1.22.07 1.82.17h.01l2.07-1.55c-1.39-.41-2.85-.62-3.9-.62z"></path>
-            </svg>}
-            label={'Teacher'}
-            sx={{
-                backgroundColor: '#eeebfe', borderRadius: 2, "& .MuiChip-label": {
-                    fontFamily: 'Google',
-                    fontWeight: 500,
-                    pl: '.5rem',
-                    color: '#44399f'
-                }
-            }}/>);
-
     return (<Chip
             avatar={<Person fill={'#3a4354'}/>}
-            label={'Student'}
+            label={'User'}
             sx={{
                 backgroundColor: '#f3f4f6', borderRadius: 2, "& .MuiChip-label": {
                     fontFamily: 'Google',
@@ -81,7 +65,7 @@ const columns = [
             return (<Box sx={{display: 'flex'}}>
                 <Avatar src={row.avatar}/>
                 <Box sx={{ml: '.5rem'}}>
-                    <Typography sx={{fontFamily: 'Google', fontWeight: 500, mb: '-.35rem'}}>{row.username}</Typography>
+                    <Typography sx={{fontFamily: 'Google', fontWeight: 500, mb: '-.35rem'}}>{row.firstName + ' ' + row.lastName}</Typography>
                     <Typography component={'a'} href={"mailto:" + row.email} sx={{
                         fontFamily: 'Google',
                         fontSize: '.8rem',
@@ -98,11 +82,8 @@ const columns = [
         headerClassName: 'app-table-header',
         flex: 1,
         sortComparator: (role1, role2) => {
-            if (role1 === 'admin') return 1;
-            if (role2 === 'admin') return -1;
-
-            if (role1 === 'teacher') return 1;
-            if (role2 === 'teacher') return -1;
+            if (role1.name === 'Admin') return 1;
+            if (role2.name === 'Admin') return -1;
         },
         renderCell: renderRole
     },
@@ -114,7 +95,7 @@ const columns = [
         renderCell: ({row}) => {
             return (
                 <>
-                    {row.role !== 'student' &&
+                    {row.role.name !== 'User' &&
                         <Tooltip title={'Not Student'}>
                             <Container sx={{
                                 display: 'flex',
@@ -130,7 +111,7 @@ const columns = [
                             }}></Container>
                         </Tooltip>
                     }
-                    {row.role === 'student' &&
+                    {row.role.name === 'User' &&
                         <TextField
                             placeholder={'Unassigned'}
                             inputProps={{
@@ -206,31 +187,17 @@ const columns = [
     }}
 ];
 
-const rows = [
-    {
-        id: 1, username: 'Snow', email: 'Jon@gmail.com', role: 'admin',
-        avatar: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg'
-    },
-    {id: 2, username: 'Lannister', email: 'Cersei@gmail.com', role: 'teacher'},
-    {id: 3, username: 'Lannister', email: 'Jaime@gmail.com', role: 'admin'},
-    {id: 4, username: 'Stark', email: 'Arya@gmail.com', role: 'teacher'},
-    {id: 5, username: 'Targaryen', email: 'Daenerys@gmail.com', role: 'student'},
-    {id: 6, username: 'Melisandre', email: null, age: 150, role: 'student'},
-    {id: 7, username: 'Clifford', email: 'Ferrara@gmail.com', role: 'student'},
-    {id: 8, username: 'Frances', email: 'Rossini@gmail.com', role: 'student'},
-    {id: 9, username: 'Roxie', email: 'Harvey@gmail.com', role: 'student'},
-    {id: 10, username: 'Snow', email: 'Jon@gmail.com', role: 'admin'},
-    {id: 12, username: 'Lannister', email: 'Cersei@gmail.com', role: 'teacher'},
-    {id: 13, username: 'Lannister', email: 'Jaime@gmail.com', role: 'admin'},
-    {id: 14, username: 'Stark', email: 'Arya@gmail.com', role: 'teacher'},
-    {id: 15, username: 'Targaryen', email: 'Daenerys@gmail.com', role: 'student'},
-    {id: 16, username: 'Melisandre', email: null, age: 150, role: 'student'},
-    {id: 17, username: 'Clifford', email: 'Ferrara@gmail.com', role: 'student'},
-    {id: 18, username: 'Frances', email: 'Rossini@gmail.com', role: 'student'},
-    {id: 11, username: 'Roxie', email: 'Harvey@gmail.com', role: 'student'},
-];
-
 export default function ManageAccountsPage() {
+    const {data } = useQuery(
+        {
+            queryKey: ["users"],
+            queryFn: async () => {
+                const response = await axios.get(`api/v1/users?limit=999`);
+                return response.data.data
+            },
+            getNextPageParam: (lastPage, allPages) => lastPage.hasNextPage
+        });
+
     return (
         <Box sx={{
             alignItems: "top",
@@ -238,7 +205,7 @@ export default function ManageAccountsPage() {
             mt: '2rem', mx: 'auto', maxWidth: '1000px'
         }}>
             <DataGrid
-                rows={rows}
+                rows={data ?? []}
                 columns={columns}
                 initialState={{
                     pagination: {
