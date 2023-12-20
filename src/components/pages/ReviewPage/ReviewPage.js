aa065d726a1cdd2d0959cf9fed9394ee801f15a2
nkhang1902 <nkhang1902@gmail.com>
2023-12-20 14:14:21 +0700

    Update ReviewPage.js

--- src/components/pages/ReviewPage/ReviewPage.js
+++ src/components/pages/ReviewPage/ReviewPage.js
@@ -1,59 +1,151 @@
 import * as React from 'react';
-import {useState} from 'react';
-import {Dialog, IconButton, useTheme} from "@mui/material";
+import {useState, useEffect} from 'react';
+import { useTheme } from '@emotion/react';
+import {Dialog, Icon, IconButton} from "@mui/material";
 import Grid from "@mui/material/Grid";
-import {ContentCopy, MoreVert, PersonAddAlt} from "@mui/icons-material";
-import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import {ReactMultiEmail} from 'react-multi-email';
-import 'react-multi-email/dist/style.css';
-import {toast, useToast} from "react-toastify";
+import {Typography, Select, MenuItem, Box} from '@mui/material';
+import { Assignment, Comment, MoreVert } from '@mui/icons-material'; 
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 export default function ReviewPage() {
-    const inviteStudentLink = 'https://a.b.com';
+  const theme = useTheme();
+  const [selectedClassId, setSelectedClassId] = useState('default');
+  const [className, setClassName] = useState('All Classes');
+  const [assignments, setAssignments] = useState([]);
 
-    const teachers = [
-        {
-            name: 'Michael',
-            avatar: 'https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J'
-        },
-        {
-            name: 'Jackson',
-            status: 'invited'
-        },
-    ]
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+    return new Intl.DateTimeFormat('en-GB', options).format(date);
+  };
 
-    const students = [
-        {
-            name: 'Phat',
-            avatar: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg'
-        },
-        {
-            name: 'Anh',
-        },
-        {
-            name: 'Nhat'
+  //get classes
+  const { data, isLoading, isError } = useQuery(
+    {
+      queryKey: ["classes"],
+      queryFn: async () => {
+        try {
+          const response = await axios.get(`api/v1/users/${localStorage.getItem('userId')}`);
+          return response.data;
+        } catch (error) {
+          throw new Error(`Error fetching data: ${error.message}`);
         }
-    ]
+      }
+    });
+  const classMemberships = data?.classMemberships?.filter(member => member.role === "teacher") ?? [];
+  const classes = classMemberships.map(member => member.class);
 
-    const theme = useTheme();
+  //get assignments for selected class
+  useEffect(() => {
+    if (selectedClassId !== 'default'){
+    const getAssignments = async () => {
+      try {
+        const response = await axios.get(`api/v1/classes/${selectedClassId}`);
+        setAssignments(response.data.assignments.filter((assignment) => !assignment.deleted));
+      } catch (error) {
+        console.error(`Error fetching assignments: ${error.message}`);
+      }
+    };
+
+    // Call getAssignments when selectedClassId changes
+    getAssignments();
+    }
+  }, [selectedClassId]);
 
 
-    return (
-        <> <Grid
-            container
-            spacing={3}
-            alignItems="top"
-            justifyContent="center"
-            sx={{margin: '0 auto', maxWidth: '1000px',}}
+  useEffect(() => {
+    console.log(assignments);
+  }, [assignments]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
+
+  const handleClassChange = (event) => {
+    const selectedId = event.target.value;
+    setSelectedClassId(selectedId);
+
+    // Find the class name based on the selected ID
+    const selectedClass = classes.find(classItem => classItem.id === selectedId);
+    setClassName(selectedClass ? selectedClass.className : 'All Classes');
+
+    // Manually trigger the assignment fetching
+  };
+
+  
+  return (
+    <Grid container spacing={3} alignItems="top" justifyContent="center" sx={{ margin: '0 auto', maxWidth: '1000px' }}>
+      <Grid item xs={12}>
+        <Select sx={{ width: '200px', fontFamily: 'Google' }}  value={selectedClassId} onChange={handleClassChange}>
+          <MenuItem value="default">All classes</MenuItem>
+          {classes.map((classItem) => (
+            <MenuItem key={classItem.id} value={classItem.id}>
+              {classItem.className}
+            </MenuItem>
+          ))}
+        </Select>
+
+        <Box
+        marginTop={2}
         >
-            <Grid item xs={12}>
-
-
-            </Grid>
-        </Grid>
-        </>
-    );
+          {assignments.map((assignment) => (
+            <Box 
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            color={theme.palette.primary.main}
+            marginBottom="1rem"
+            sx={{ borderBottom: '0.5px solid',
+                  ':hover': {
+                    backgroundColor: '#e1f0fc', 
+                    borderRadius:'8px'
+                  }, }}>
+              <Box display="flex" alignItems="center" padding='8px' >
+                  <Box    bgcolor={theme.palette.primary.main} 
+                          alignContent='center'
+                          paddingX="0.4rem" 
+                          paddingTop='6px'
+                          marginRight='1rem'
+                          borderRadius={50}
+                          color="#FFFFFF" 
+                          >
+                    <Icon component={Assignment} fontSize="medium" />
+                  </Box>
+                  <Box>
+                    <Typography sx={{ fontFamily: 'Google', fontWeight:500 }}>{assignment.name}</Typography>
+                    <Typography sx={{ fontFamily: 'Google', fontWeight:100, fontSize:12}}>Due {formatDate(assignment.dueDate)}</Typography>
+                  </Box>
+              </Box>
+              <Box display="flex" alignItems="center" padding='8px' >
+                <Box display="flex" alignItems="center" >
+                  <Box
+                            alignContent='center'
+                            paddingX="0.4rem" // Adjust padding as needed
+                            paddingTop='6px'
+                            borderRadius={50} // Adjust border radius as needed
+                            color={theme.palette.primary.main} 
+                            >
+                      <Icon component={Comment} fontSize="medium" />
+                    </Box>
+                    <Box>
+                      <Typography sx={{ fontFamily: 'Google', fontWeight:500 }}>3 to Review</Typography>
+                    </Box>
+                </Box>
+                <Box>
+                  <IconButton>
+                  <Icon component={MoreVert} fontSize="medium" />
+                  </IconButton>
+                </Box>
+              </Box>
+            </Box>
+            ))}
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
