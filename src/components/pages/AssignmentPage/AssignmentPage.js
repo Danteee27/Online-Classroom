373ce848f7a696743464851562728daf139f73cc
nkhang1902 <nkhang1902@gmail.com>
2023-12-25 19:25:03 +0700

    Update AssignmentPage.js

--- src/components/pages/AssignmentPage/AssignmentPage.js
+++ src/components/pages/AssignmentPage/AssignmentPage.js
@@ -11,10 +11,12 @@ import TextField from "@mui/material/TextField";
 import {useParams} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
+import { useQueryClient } from '@tanstack/react-query';
+import {toast} from "react-toastify";
+import { useEffect } from 'react';
 
 export default function AssignmentPage() {
     const {classId, assignmentId, membershipId} = useParams();
-
     const {data: classDetails} = useQuery(
         {
             queryKey: ["class", classId],
@@ -23,6 +25,7 @@ export default function AssignmentPage() {
                 return response.data
             }
         });
+    
 
     const { data: details } = useQuery({
         queryKey: ["assignmentDetails", classId, membershipId, assignmentId],
@@ -31,9 +34,7 @@ export default function AssignmentPage() {
             return response.data;
         },
     });
-
     const assignment = classDetails?.assignments?.find(assignment => assignment.id.toString() === assignmentId);
-
     const {data: creator} = useQuery(
         {
             queryKey: ["user", assignment?.creator?.id],
@@ -47,9 +48,9 @@ export default function AssignmentPage() {
 
     const a = {
         name: assignment?.name,
-        createdAt: assignment?.createdDate && new Date(assignment?.createdDate).toDateString(),
+        createdAt: details?.createdAt && new Date(details?.createdAt).toDateString(),
         maxGrade: assignment?.maxGrade,
-        description: assignment?.description,
+        description: details?.description,
         studentName: details?.classMembership?.fullName ?? details?.classMembership?.user?.firstName + ' ' +details?.classMembership?.user?.lastName,
         teacherName: creator?.firstName + ' ' + creator?.lastName,
         studentExplanation: details?.studentExplanation,
@@ -62,16 +63,60 @@ export default function AssignmentPage() {
         isFinalised: details?.isFinalised,
         isRequested: details?.isRequested,
         isReviewed: details?.isReviewed,
-        isSubmitted: details?.isSubmitted,
+        isSubmitted: details?.isSubmitted
     }
-
+    const [reviewData, setReviewData] = useState({
+        createdAt: details?.createdAt && new Date(details?.createdAt).toDateString(),
+        studentName: details?.classMembership?.fullName ?? details?.classMembership?.user?.firstName + ' ' +details?.classMembership?.user?.lastName,
+        studentExplanation: details?.studentExplanation,
+        teacherComment: details?.teacherComment,
+        studentComment: details?.studentComment,
+        teacherFinalisedComment: details?.teacherFinalisedComment,
+        grade: '',
+        currentGrade: details?.currentGrade ?? 0,
+        expectedGrade: details?.expectedGrade ?? 0,
+        isFinalised: details?.isFinalised,
+        isRequested: details?.isRequested,
+        isReviewed: details?.isReviewed,
+        isSubmitted: details?.isSubmitted,
+        // Add other fields as needed
+      });
+    
+      const handleFieldChange = (fieldName) => (e) => {
+        setReviewData((prevData) => ({
+          ...prevData,
+          [fieldName]: e.target.value,
+        }));
+      };
     const sendReview = () => {
         // todo handle post review
     }
-
+    const queryClient = useQueryClient();
     const theme = useTheme();
     const [review, setReview] = useState("");
-
+    const handleEditAssignment = async (role) => {
+        console.log(reviewData)
+        try {
+          if(role === 'student'){
+            if(reviewData.isRequested === false) {setReviewData({...reviewData, isRequested: true})}
+            else {toast.error("You have already requested for review!");
+            return;}
+          }
+          else if(role === 'teacher'){
+            if(reviewData.isRequested === true) {setReviewData({...reviewData, isRequested: false})}
+            else {toast.error("You have already reviewed this assignment!");
+            return;}
+          }
+          const response = await axios.put( `/api/v1/classes/${classId}/classMemberships/${membershipId}/assignment/${assignmentId}`, reviewData);
+          console.log(response)
+          
+          // Refetch the data after deletion
+          await queryClient.refetchQueries();
+          toast.success("Assignment updated successfully!");
+        } catch (e) {
+          toast.error(e.message);
+        }
+      };
     const leftColumn = (
         <Box sx={{display: 'flex'}}>
             <Box sx={{
@@ -102,16 +147,25 @@ export default function AssignmentPage() {
                                     fontSize: '.875rem',
                                 }}>{a.createdAt}</Typography>
                 </Box>
-                <Typography variant={'subtitle2'}
-                            sx={{
-                                fontFamily: 'Google',
-                                fontSize: '.875rem',
-                                borderBottom: '0.0625rem solid ' + theme.palette.primary.main,
-                                mb: '1rem',
-                                pb: '1rem'
-                            }}>
-                    <b>{a.grade}</b>/{a.maxGrade}
-                </Typography>
+                <Box sx={{ mb: '1rem', borderBottom: '0.0625rem solid lightgrey',}}>
+                    <Typography variant={'subtitle2'}
+                                sx={{
+                                    fontFamily: 'Google',
+                                    fontSize: '.875rem',
+                                }}>
+                        <b>{a.grade}</b>/100
+                    </Typography>
+                    { !a.isRequested && (
+                    <Typography variant={'subtitle2'}
+                                sx={{
+                                    fontFamily: 'Google',
+                                    fontSize: '.875rem',
+                                    color: "red",
+                                    mb: '1rem',
+                                }}>
+                        Expected Grade: {a.expectedGrade}
+                    </Typography>)}
+                </Box>
                 <Typography variant={'body1'}
                             sx={{
                                 fontSize: '.875rem',
@@ -128,7 +182,7 @@ export default function AssignmentPage() {
                                     fontFamily: 'Google',
                                     color: theme.palette.primary.main
                                 }}>
-                        Student reviews
+                        Comments
                     </Typography>
                 </Box>
                 {a.studentExplanation && <ChatDialog name={a.studentName} reason={a.studentExplanation}/>}
@@ -177,46 +231,134 @@ export default function AssignmentPage() {
             </Box>
         </Box>
     }
+    const RightColumn = () => {
+        const [classMembership, setClassMembership] = useState(null);
+        const userId = localStorage.getItem("userId").toString();
+        const {classId} = useParams();
+        console.log(userId)
+        const {data: userDetails} = useQuery(
+            {
+                queryKey: ["user", userId],
+                queryFn: async () => {
+                    const response = await axios.get(`api/v1/users/${userId}`);
+                    return response.data
+                }
+        });
 
-    const rightColumn = (
-        <Box>
+        useEffect(() => {
+            // Assuming userDetails is fetched successfully
+            const membership = userDetails?.classMemberships?.find(member => member.class.id === Number(classId));
+            setClassMembership(membership);
+          }, [userDetails, classId]);        
+        const isTeacher = classMembership?.role === 'teacher';
+        const isStudent = classMembership?.role === 'student';
+        return ( <Box>
+        {isTeacher &&  (<Box>
+        <Box sx={{
+            boxShadow: 3,
+            p: 2,
+            border: '0.0625rem solid rgb(218,220,224)',
+            borderRadius: 2,
+        }}>
             <Box sx={{
-                boxShadow: 3,
-                p: 2,
-                border: '0.0625rem solid rgb(218,220,224)',
-                borderRadius: 2,
-            }}>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center', mt: '0.5rem'
-                }}><Typography
-                    variant={'h5'}
-                    sx={{fontColor: 'rgba(0,0,0,.549)', fontFamily: 'Google'}}>
-                    Your work
-                </Typography>
-                    <Typography
-                        variant={'h6'}
-                        sx={{fontSize: '.875rem', fontColor: 'rgba(0,0,0,.549)', fontFamily: 'Google'}}>
-                        Graded
-                    </Typography></Box>
-                <Button variant={'outlined'} sx={{textTransform: 'none', width: '100%', mt: '1rem'}}>
-                    <Add/>
-                    <Typography sx={{fontSize: '.875rem', fontFamily: 'Google', fontWeight: 500}}>Add or
-                        create</Typography>
-                </Button>
-                <Button variant={'contained'} sx={{textTransform: 'none', width: '100%', my: '1rem'}}>
-                    <Typography sx={{
-                        fontSize: '.875rem',
-                        fontFamily: 'Google',
-                        fontWeight: 500,
-                        color: 'white'
-                    }}>Resubmit</Typography>
-                </Button>
-            </Box>
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center', mt: '0.5rem'
+            }}><Typography
+                variant={'h5'}
+                sx={{fontColor: 'rgba(0,0,0,.549)', fontFamily: 'Google', fontWeight: 500}}>
+                Review
+                 </Typography>
+                </Box>
+                <TextField
+                label="Adjusted Grade"
+                fullWidth
+                type="number"
+                value={reviewData.grade}
+                onChange={(e) => setReviewData({ ...reviewData, grade: Number(e.target.value) })}
+                margin="normal"
+                />
+                {a.teacherComment === null ? (
+                    <TextField
+                    label="Comment"
+                    fullWidth
+                    onChange={(e) => setReviewData({ ...reviewData, teacherComment: e.target.value })}
+                    margin="normal"
+                    />
+                ) : (
+                    <TextField
+                    label="Comment"
+                    fullWidth
+                    onChange={(e) => setReviewData({ ...reviewData, teacherFinalisedComment: e.target.value })}
+                    margin="normal"
+                    />
+                )}
+            
+            <Button variant={'contained'} sx={{textTransform: 'none', width: '100%', my: '1rem'}}
+            onClick={()=>handleEditAssignment()}>
+                <Typography sx={{
+                    fontSize: '.875rem',
+                    fontFamily: 'Google',
+                    fontWeight: 500,
+                    color: 'white'
+                }}>Publish review</Typography>
+            </Button>
+        </Box>
 
 
-        </Box>)
+    </Box>)} {isStudent &&
+    (<Box>
+        <Box sx={{
+            boxShadow: 3,
+            p: 2,
+            border: '0.0625rem solid rgb(218,220,224)',
+            borderRadius: 2,
+        }}>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center', mt: '0.5rem'
+            }}><Typography
+                variant={'h5'}
+                sx={{fontColor: 'rgba(0,0,0,.549)', fontFamily: 'Google', fontWeight: 500}}>
+                Request for review
+                 </Typography>
+                </Box>
+                <TextField
+                label="Expected Grade"
+                fullWidth
+                type="number"
+                value={reviewData.grade}
+                onChange={(e) => setReviewData({ ...reviewData, expectedGrade: Number(e.target.value) })}
+                margin="normal"
+                />
+                {a.teacherComment === null ? (
+                    <TextField
+                    label="Comment"
+                    fullWidth
+                    onChange={(e) => setReviewData({ ...reviewData, studentExplanation: e.target.value })}
+                    margin="normal"
+                    />
+                ) : (
+                    <TextField
+                    label="Comment"
+                    fullWidth
+                    onChange={(e) => setReviewData({ ...reviewData, studentComment: e.target.value })}
+                    margin="normal"
+                    />
+                )}
+            
+            <Button variant={'contained'} sx={{textTransform: 'none', width: '100%', my: '1rem'}}
+            onClick={()=>handleEditAssignment(classMembership?.role)}>
+                <Typography sx={{
+                    fontSize: '.875rem',
+                    fontFamily: 'Google',
+                    fontWeight: 500,
+                    color: 'white'
+                }}>Publish request</Typography>
+            </Button>
+        </Box>
+    </Box>)}</Box> );};
 
     return (<Grid
             container
@@ -226,7 +368,7 @@ export default function AssignmentPage() {
             sx={{margin: '0 auto', mx: 'auto', maxWidth: '1000px',}}
         >
             <Grid item xs={8}>{leftColumn} </Grid>
-            <Grid item xs={4}>{rightColumn}</Grid>
+            <Grid item xs={4}>{RightColumn()}</Grid>
         </Grid>
     );
 }
